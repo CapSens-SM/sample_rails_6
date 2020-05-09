283c25fa21685081d04f8d1af3b08c9e4f5b31ed
CapSens-SM <contact@capsens.eu>
2020-05-09 12:49:13 +0200

    Adding draper and decoration logic

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,21 +1,18 @@
 ActiveAdmin.register Customer do
-  config.per_page = 12
+  decorate_with CustomerDecorator
 
   index do
     selectable_column
     column :id
-    column :firstname
-    column :lastname
+    column :name, sortable: :lastname
     column :email
     column :phone_number
     column :city
     column :language
-    column :membership_amount
+    column :membership_amount_in_dollars, sortable: :membership_amount
     column :state
     column :created_at
     column :updated_at
     actions
   end
-
-  
 end
--- app/decorators/customer_decorator.rb
+++ app/decorators/customer_decorator.rb
@@ -1,8 +1,12 @@
 class CustomerDecorator < Draper::Decorator
   delegate_all
 
-  def display_name
+  def name
     "#{firstname&.capitalize} #{lastname&.upcase}"
+  end
+
+  def membership_amount_in_dollars
+    h.number_to_currency(membership_amount, locale: :en)
   end
 
 end
